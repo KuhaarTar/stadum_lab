60b2752294b3ae48362fe089cac4ebb26d7a5a8b
KuhaarTar <flempiefilms@gmail.com>
2023-03-13 11:16:47 +0200

    added private modificators and for each loop

--- src/main/java/ua/lviv/iot/algo/part1/stadiumLab/Stadium.java
+++ src/main/java/ua/lviv/iot/algo/part1/stadiumLab/Stadium.java
@@ -1,67 +1,60 @@
 package ua.lviv.iot.algo.part1.stadiumLab;
+
 import lombok.*;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-
+@ToString
 public class Stadium {
-    String name;
-    int capacity;
-    int currentAttendance;
-    String homeTeam;
-    String awayTeam;
-    static Stadium instance;
+    private String name;
+    private int capacity;
+    private int currentAttendance;
+    private String homeTeam;
+    private String awayTeam;
+    private static Stadium instance;
+
     static Stadium getInstance() {
         if (instance == null) {
             instance = new Stadium();
         }
         return instance;
     }
-    void addAttendies(int count){
-        if ( currentAttendance!=capacity ) {
+
+    void addAttendies(int count) {
+        if (currentAttendance != capacity) {
             currentAttendance += count;
         } else {
             System.out.println("Stadium is full");
         }
     }
-    void decreaseAttendance () {
-        if (currentAttendance-100<0){
-            currentAttendance=0;
+
+    void decreaseAttendance() {
+        if (currentAttendance - 100 < 0) {
+            currentAttendance = 0;
         } else {
-            currentAttendance=currentAttendance-100;
+            currentAttendance = currentAttendance - 100;
         }
     }
+
     void changeHomeTeam(String teamName) {
-        homeTeam=teamName;
-    }
-    void changeAwayTeam(String teamName){
-        awayTeam=teamName;
+        homeTeam = teamName;
     }
 
-    @Override
-    public String toString() {
-        return "Stadium{" +
-                "name='" + name + '\'' +
-                ", capacity=" + capacity +
-                ", currentAttendance=" + currentAttendance +
-                ", homeTeam='" + homeTeam + '\'' +
-                ", awayTeam='" + awayTeam + '\'' +
-                '}';
+    void changeAwayTeam(String teamName) {
+        awayTeam = teamName;
     }
+
 
     public static void main(String[] args) {
-        int icrement=0;
-
-        Stadium [] objects = new Stadium[4];
-        objects[0]= new Stadium();
-        objects[1]= new Stadium("CityStadium",1000,450,"Blue team","RedTeam");
-        objects[2]= getInstance();
-        objects[3]= getInstance();
-
-        while (icrement>=0 && icrement< objects.length) {
-            System.out.println(objects[icrement].toString());
-            icrement++;
+        var stadiums = new Stadium[4];
+        stadiums[0] = new Stadium();
+        stadiums[1] = new Stadium("CityStadium", 1000, 450, "Blue team", "RedTeam");
+        stadiums[2] = getInstance();
+        stadiums[3] = getInstance();
+        for (Stadium stadium : stadiums) {
+            System.out.println(stadium);
         }
     }
 }
