284248a37bbfb4d9a0c5e8ff1d4c3510ceabe5cf
KuhaarTar <flempiefilms@gmail.com>
2023-05-07 20:13:25 +0300

    create REST API using spring

---
+++ src/main/java/ua/lviv/iot/algo/part2/java_spring_lab/JavaSpringLabApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.algo.part2.java_spring_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/lviv/iot/algo/part2/java_spring_lab/JavaSpringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.algo.part2.java_spring_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
